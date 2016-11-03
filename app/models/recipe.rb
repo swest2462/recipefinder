6c8d8299a4705942526ac4b7d5f5308c69a17903
Aleksandr Vest <swest2462@gmail.com>
2016-11-03 11:34:18 +0300

    added search form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,8 @@ class Recipe < ActiveRecord::Base
   include HTTParty
   key_value = ENV['FOOD2FORK_KEY'] || '1805e728325047a23755b19d8114fe1d'
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
-  base_uri "http://food2fork.com/api"
+  FOOD2FORK_SERVER_AND_PORT = 'food2fork.com'
+  base_uri "http://#{hostport}/api"
   default_params key: key_value
   format :json
 
